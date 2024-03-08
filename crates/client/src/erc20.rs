4963fdf2ce516bc52990f726ae9596f8db61f28f
jiang <fenghaojiang97@gmail.com>
2024-03-08 13:30:54 +0800

    chore: add more tests

--- crates/client/src/erc20.rs
+++ crates/client/src/erc20.rs
@@ -25,15 +25,18 @@ abigen!(
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn test_on_erc20_call() -> Result<()> {
-    let provider = Provider::<Http>::try_from("https://rpc.ankr.com/eth")?;
-    let provider_arc = Arc::new(provider);
+    let provider_arc = Arc::new(Provider::<Http>::try_from("https://rpc.ankr.com/eth")?);
     let birdring_account = "0x162c6270266667ccf5a9ed752b5d6a2bdc0f90de".parse()?;
     let rss3_token: Address = "0xc98d64da73a6616c42117b582e832812e7b8d57f".parse()?;
+    let usdt_token: Address = "0xdAC17F958D2ee523a2206206994597C13D831ec7".parse()?;
+    let provider_usdt = provider_arc.clone();
+
     let erc20_contract = Arc::new(IERC20::new(rss3_token, provider_arc));
+    let usdt_contract = Arc::new(IERC20::new(usdt_token, provider_usdt.clone()));
 
     let rss3_decimals = erc20_contract.decimals().call().await?;
-    let rss3_name = erc20_contract.decimals().call().await?;
-    let rss3_symbol = erc20_contract.decimals().call().await?;
+    let rss3_name = erc20_contract.name().call().await?;
+    let rss3_symbol = erc20_contract.symbol().call().await?;
     let rss3_total_supply = erc20_contract.total_supply().call().await?;
     let rss3_balance = erc20_contract.balance_of(birdring_account).call().await?;
 
@@ -41,6 +44,13 @@ async fn test_on_erc20_call() -> Result<()> {
     println!("rss3 name: {}", rss3_name);
     println!("rss3 symbol: {}", rss3_symbol);
     println!("rss3 total supply: {}", rss3_total_supply);
+
+
+    let usdt_name = usdt_contract.name().call().await?;
+    let usdt_symbol = usdt_contract.symbol().call().await?;
+
+    println!("usdt name: {}", usdt_name);
+    println!("usdt symbol: {}", usdt_symbol);
 
     println!("birdring rss3 balance: {}", rss3_balance);
     Ok(())
